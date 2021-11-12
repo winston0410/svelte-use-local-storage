d4a0ba909fc70b8123f37246b78e56513e63efc3
kghugo <hugosum.dev@protonmail.com>
2021-11-12 08:35:56 +0000

    Acknowledge esm issue

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,7 +3,7 @@ import esbuild from "esbuild";
 esbuild
   .build({
     entryPoints: ["index.ts"],
-    bundle: true,
+    bundle: false,
     outfile: "./dist/index.js",
   })
   .catch(() => process.exit(1));
--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-import storageOk from 'storage-available'
+// REF https://github.com/sveltejs/kit/issues/1549
+// NOTE Importing cjs module is broken right now in Vite
+//  import storageOk from 'storage-available'
+
 //  Taken from this repo, as these types should be merged to core later
 //  https://github.com/paolotiu/svelte-useactions/blob/main/src/index.ts
 /**
@@ -26,6 +29,21 @@ export type Action<
 > = (node: Node, params?: Params) => ActionReturn<Params>;
 
 type InputElement = HTMLInputElement | HTMLTextAreaElement
+
+type Storage = "localStorage" | "sessionStorage"
+
+const storageOk = (type: Storage) => {
+	try {
+		var storage = window[type],
+			x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e) {
+		return false;
+	}
+}
 
 const useLocalStorage: Action<InputElement, string> = (node, keyName) => {
   if (!storageOk('localStorage')) {
