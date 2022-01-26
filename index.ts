ba217eae4b096f65277827b5872d93c031969c26
kghugo <hugosum.dev@protonmail.com>
2022-01-26 16:45:37 +0000

    Handle radio button

--- index.ts
+++ index.ts
@@ -23,47 +23,77 @@ export type ActionLike<Node extends HTMLElement> = (
 /**
  * A primitive Action
  */
-export type Action<
-  Node extends HTMLElement = HTMLElement,
-  Params = unknown
-> = (node: Node, params?: Params) => ActionReturn<Params>;
+export type Action<Node extends HTMLElement = HTMLElement, Params = unknown> = (
+  node: Node,
+  params?: Params
+) => ActionReturn<Params>;
 
-type InputElement = HTMLInputElement | HTMLTextAreaElement
+type GroupElement = HTMLFieldSetElement;
+type InputElement = HTMLInputElement | HTMLTextAreaElement;
+type PersistableElement = InputElement | GroupElement;
 
-type Storage = "localStorage" | "sessionStorage"
+type Storage = "localStorage" | "sessionStorage";
 
 const storageOk = (type: Storage) => {
-	try {
-		var storage = window[type],
-			x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch(e) {
-		return false;
-	}
-}
-
-const useLocalStorage: Action<InputElement, string> = (node, keyName) => {
-  if (!storageOk('localStorage')) {
-      console.warn("localStorage is not supported by the current browser.")
-      return
+  try {
+    var storage = window[type],
+      x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return false;
   }
-  const saved = localStorage.getItem(keyName)
-  node.value = saved
+};
+
+type ValueSetter<T extends PersistableElement> = (
+  node: T,
+  keyName: string
+) => void;
+
+const setInput: ValueSetter<InputElement> = (node, keyName) => {
+  const saved = localStorage.getItem(keyName);
+  node.value = saved;
+};
+
+const setRadio: ValueSetter<GroupElement> = (node, keyName) => {
+  const savedKey = localStorage.getItem(keyName);
+  const targetButton = ([...node.elements] as Array<HTMLInputElement>).find(
+    (x) => x.value === savedKey
+  );
+
+  if (targetButton) {
+    targetButton.checked = true;
+  }
+};
+
+const useLocalStorage: Action<PersistableElement, string> = (node, keyName) => {
+  if (!storageOk("localStorage")) {
+    console.warn("localStorage is not supported by the current browser.");
+    return;
+  }
+
+  switch (node.tagName.toLowerCase()) {
+    case "fieldset":
+      setRadio(node as GroupElement, keyName);
+      break;
+
+    default:
+      setInput(node as InputElement, keyName);
+      break;
+  }
 
   const handleChange = (e: InputEvent) => {
-      localStorage.setItem(keyName, (e.target as HTMLInputElement).value)
-  }
-  
-  node.addEventListener('change', handleChange)
-  
+    localStorage.setItem(keyName, (e.target as HTMLInputElement).value);
+  };
+
+  node.addEventListener("change", handleChange);
+
   return {
     destroy() {
-        node.removeEventListener('change', handleChange)
+      node.removeEventListener("change", handleChange);
     },
   };
 };
 
-export default useLocalStorage
+export default useLocalStorage;
