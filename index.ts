402e568f1ade67d4cafc4deb52232fb50a4c7ae7
kghugo <hugosum.dev@protonmail.com>
2022-01-27 17:50:21 +0000

    Extract action type

--- index.ts
+++ index.ts
@@ -1,32 +1,7 @@
 // REF https://github.com/sveltejs/kit/issues/1549
 // NOTE Importing cjs module is broken right now in Vite
 //  import storageOk from 'storage-available'
-
-//  Taken from this repo, as these types should be merged to core later
-//  https://github.com/paolotiu/svelte-useactions/blob/main/src/index.ts
-/**
- * The return type of an action.
- */
-export type ActionReturn<Params> = {
-  destroy?: () => void;
-  update?: (params: Params) => void;
-} | void;
-
-/**
- * Action type shim
- */
-export type ActionLike<Node extends HTMLElement> = (
-  node: Node,
-  params: unknown
-) => unknown;
-
-/**
- * A primitive Action
- */
-export type Action<Node extends HTMLElement = HTMLElement, Params = unknown> = (
-  node: Node,
-  params?: Params
-) => ActionReturn<Params>;
+import type { Action } from 'svelte-action-type'
 
 type GroupElement = HTMLFieldSetElement;
 type InputElement = HTMLInputElement | HTMLTextAreaElement;
